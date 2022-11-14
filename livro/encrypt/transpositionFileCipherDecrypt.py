52201308b977092cd6cbd36d16b45bd6153fbdb9
Pedro Hoppe <pedrohbh@users.noreply.github.com>
2022-11-14 15:57:34 -0300

    Adição de exemplos de criptografia

---
+++ livro/encrypt/transpositionFileCipherDecrypt.py
@@ -0,0 +1,42 @@
+import time, os, sys, transpositionEncrypt, transpositionDecrypt
+
+def main():
+    inputFilename = 'frankenstein.encrypted.txt'
+
+    outputFilename = 'frankenstein.decrypted.txt'
+    myKey = 10
+    myMode = 'decrypt'
+
+    if not os.path.exists(inputFilename):
+        print('The file %s does not exist. Quitting...' % (inputFilename))
+        sys.exit()
+
+    if os.path.exists(outputFilename):
+        print('This will overwrite the file %s. (C)ontinue or (Q)uit?' % (outputFilename))
+        response = input('> ')
+        if not response.startswith('c'):
+            sys.exit()
+
+    fileObj = open(inputFilename)
+    content = fileObj.read()
+    fileObj.close()
+
+    print('%sing...' % (myMode.title()))
+
+    startTime = time.time()
+    if myMode == 'encrypt':
+        translated = transpositionEncrypt.encryptMessage(myKey, content)
+    elif myMode == 'decrypt':
+        translated = transpositionDecrypt.decryptMessage(myKey, content)
+    totalTime = round(time.time() - startTime, 2)
+    print('%sion time: %s seconds' % (myMode.title(), totalTime))
+
+    outputFileObj = open(outputFilename, 'w')
+    outputFileObj.write(translated)
+    outputFileObj.close()
+
+    print('Done %sing %s (%s characters).' % (myMode, inputFilename, len(content)))
+    print('%sed file is %s.' % (myMode.title(), outputFilename))
+
+if __name__ == '__main__':
+    main()
